e9acacfdaf19f33b6f5a25501db1c1e7834848ff
vanistillhere <miguel.gandeza@gmail.com>
2025-08-24 23:57:51 +0800

    lay out framework main.js

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-// imports first
+/*
+IDEA:
+    use the spotify web api to request spotify playlist info
+    import it into a .json file with the structure
+
+    {
+        "playlistID": {
+            "name": "playlistName",
+            "songs": [
+                {
+                    "songName": "songName",
+                    "songID": "songID",
+                    "length": "songLength",
+                    "artist": "songArtist"
+                }
+            ]
+        }    
+    }
+
+*/
+
+// imports
+const [
+    fs,
+    readline,
+    YTMusic,
+    colors
+] = [
+    require("fs"),
+    require("readline"),
+    require("ytmusic-api"),
+    require("colors")
+]
+async function importPlaylist(params) {
+    
+}
+
+
+async function getPlaylist(params) {
+    
+}
+
+async function getAccess(params) {
+    
+}
+
